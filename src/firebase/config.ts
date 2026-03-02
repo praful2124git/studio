b10406092a78bcf6857b0a574b0e59588cf6877e
sam Shock <samshock023@gmail.com>
2026-03-02 14:20:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9505398502-55341",
+  "appId": "1:602597153101:web:aa7e29abde9a73f1797f04",
+  "apiKey": "AIzaSyD-m2XnDQhz0zlsWubd5aO40fInoU7LyRU",
+  "authDomain": "studio-9505398502-55341.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "602597153101"
+};
